5d7f22f086976d56cb378a97cf1e2266df3df36f
chieh <chieh@25sprout.com>
2024-09-23 20:06:31 +0800

    Impelement evaluatePolishNotation.py

--- 150-evaluatePolishNotation.py
+++ 150-evaluatePolishNotation.py
@@ -1,3 +1,24 @@
 class Solution:
+    # Time: O(2n) => O(n)
+    # Space: O(n)
+
     def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            else:
+                # Truncation (with int()): It simply cuts off the decimal part, 
+                # floor() rounds down to the nearest integer.
+                stack.append(int(c))
         
+        return stack[0]
