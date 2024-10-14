b83ac8b4bca65a39d4d1e02473ce13a4f2bc2aa8
chieh <chieh@25sprout.com>
2024-10-14 14:15:09 +0800

    Impelement the 424 again and need to see the solution again but I fiqure out the solution by just read the logic not the whole code and tutorial which is a big improvment =D

--- 424-longestRepeatingCharacterReplacement.py
+++ 424-longestRepeatingCharacterReplacement.py
@@ -23,3 +23,36 @@ class Solution(object):
 
             res = max(res, r - 1 + 1)
         return res
+    
+    class Solution:
+        def characterReplacement(self, s: str, k: int) -> int:
+            # Create a defaultdict with a default value of an empty integer
+            sub = defaultdict(int)
+            l, r = 0, 0
+            res = 0
+
+            while r <= len(s) - 1:
+                # 1. we need to build the sub dictionary to count every elements in s
+                # 2. we use the windowLen subtract the most frequent ele and check is it smaller and equal to k,
+                # in that case the window would be valid
+                # if the window is invalid we increment the l pointer
+                # if in does valid we update the res and move on to the next r till the end of the s string
+
+                windowLen = r - l + 1
+
+                sub[s[r]] += 1
+                
+                # the most important point in this question is to find the valid window
+                if windowLen - max(sub.values()) <= k:
+                    res = max(res, windowLen)
+                else:
+                    sub[s[l]] -= 1
+                    l += 1
+                
+                r += 1
+            
+            return res
+        
+        # if the s = ABAACSASVF, k =2
+        # what would be the sub looks like?
+        # sub = {A: 3, B: 1, C: 1, S: 2, V: 1, F: 1}
