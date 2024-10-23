810cc4b821ff3683945d5ee3c12fd8a123dba931
chieh <chieh@25sprout.com>
2024-10-23 15:49:34 +0800

    add another comment version for 76

--- 76-minimumWindowSubstring.py
+++ 76-minimumWindowSubstring.py
@@ -58,3 +58,54 @@ class Solution:
         # s[l:r+1] meaning start from index l and go up to (and including) index r. The +1 is used to include the character at index r in the slice.
         # if resLen is still float('inf') then return an empty string, meaning no valid window was found.
         return s[l:r+1] if resLen != float('inf') else ""
+
+from collections import defaultdict
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        # first thing is handling edge cases
+        if t == '':
+            return ''
+
+        # second setup hashMap variables
+        window, tCount = defaultdict(int), defaultdict(int)
+
+        # third setup the tCount
+        for c in t:
+            tCount[c] += 1
+
+        # fourth variables for keeping track of how many characters we need
+        have, need = 0, len(tCount)
+        res = [-1, -1]
+        resLen = float("inf")
+        l = 0
+        
+        # fifth loop through the string s and create the window hashMap
+        for r in range(len(s)):
+            c = s[r]
+            window[c] += 1
+
+            # sixth deal with the increment of "have" value when valid
+            if c in tCount and window[c] == tCount[c]:
+                have += 1
+            
+            # seventh handle the shrinking of the window when valid
+            while have == need:
+                # update result if this window is smaller
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                
+                # eighth try to shrink the window from the leftmost element
+                window[s[l]] -= 1
+
+                # ninth adjust "have" if the removed character is in tCount and the window no longer satisfies
+                if s[l] in tCount and window[s[l]] < tCount[s[l]]:
+                    have -= 1
+
+                l += 1
+
+        # tenth deconstruct the result to return the correct substring
+        l, r = res
+        return s[l:r+1] if resLen != float("inf") else ""
